a0b21609499a378dce0b032da4ac4fcbc5ff3aa5
alstjr7375 <alstjr7375@daum.net>
2022-02-27 07:24:59 +0900

    Clean: Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,101 @@
+import { List } from "ts-toolbelt";
+
 // https://stackoverflow.com/questions/23475372/extrapolate-split-cubic-bezier-to-1-1
 // https://www.the-art-of-web.com/css/timing-function/
 // https://pomax.github.io/bezierinfo/
+
+// == Types ====================================================================
 type bezierCoordT = [number, number, number, number];
+type bezierUnitedCoordT = List.Flatten<List.Zip<cubicBezierCoordsI['x'], cubicBezierCoordsI['y']>>;
+
 interface cubicBezierCoordsI {
   x: bezierCoordT;
   y: bezierCoordT;
 }
-
 interface splicCubicBezierOptionI extends cubicBezierCoordsI {
   z: number;
   fitUnitSquare: boolean;
 }
 
-export function splitCubicBezier(options: splicCubicBezierOptionI) {
-  var z = options.z,
-      cz = z-1,
-      z2 = z*z,
-      cz2 = cz*cz,
-      z3 = z2*z,
-      cz3 = cz2*cz,
-      x = options.x,
-      y = options.y;
+interface splicCubicBezierResultI {
+  left: bezierUnitedCoordT;
+  right: bezierUnitedCoordT;
+}
 
-  var left = [
+// == Fit Unit Square ==========================================================
+function fitCoord(el: bezierUnitedCoordT[number], selfCoord: bezierUnitedCoordT, minI: number, maxI: number): bezierUnitedCoordT[number] {
+  const coordMin = selfCoord[minI];    // should be 0
+  const coordMax = selfCoord[maxI];    // should be 1
+  const coordS   = 1 / (coordMax - coordMin);
+  return (el - coordMin) * coordS;
+}
+
+function fitUnit(el: bezierUnitedCoordT[number], index: number, selfCoord: bezierUnitedCoordT): bezierUnitedCoordT[number] {
+  if((index % 2) === 0) {
+    // xval:  return el * (1 / selfCoord[6])
+    return fitCoord(el, selfCoord, 0, 6);
+  }
+  else {
+    // yval:  return el * (1 / selfCoord[7])
+    return fitCoord(el, selfCoord, 1, 7);
+  }
+}
+
+function fitUnitMap(coord: bezierUnitedCoordT): bezierUnitedCoordT {
+  // For typecheck. Array.map(value, index, array)'s array type is just number[]
+  const result = [];
+  for (const [el, index] of coord.entries()) {
+    result.push(fitUnit(el, index, coord));
+  }
+  return result as bezierUnitedCoordT;
+}
+
+function fitUnitSquare({left, right}: splicCubicBezierResultI): splicCubicBezierResultI {
+  return {
+    left: fitUnitMap(left),
+    right: fitUnitMap(right)
+  };
+}
+
+// == Split Cubic Bezier =======================================================
+export function splitCubicBezier(options: splicCubicBezierOptionI): splicCubicBezierResultI {
+  const x = options.x,
+        y = options.y,
+
+       z1 = options.z,
+       z2 =  z1 *  z1,
+       z3 =  z2 *  z1,
+
+      cz1 =  z1 -   1,
+      cz2 = cz1 * cz1,
+      cz3 = cz2 * cz1;
+
+  const left: bezierUnitedCoordT = [
     x[0],
     y[0],
-    z*x[1] - cz*x[0],
-    z*y[1] - cz*y[0],
-    z2*x[2] - 2*z*cz*x[1] + cz2*x[0],
-    z2*y[2] - 2*z*cz*y[1] + cz2*y[0],
-    z3*x[3] - 3*z2*cz*x[2] + 3*z*cz2*x[1] - cz3*x[0],
-    z3*y[3] - 3*z2*cz*y[2] + 3*z*cz2*y[1] - cz3*y[0]];
+    z1*x[1] - cz1*x[0],
+    z1*y[1] - cz1*y[0],
+    z2*x[2] - 2*z1*cz1*x[1] + cz2*x[0],
+    z2*y[2] - 2*z1*cz1*y[1] + cz2*y[0],
+    z3*x[3] - 3*z2*cz1*x[2] + 3*z1*cz2*x[1] - cz3*x[0],
+    z3*y[3] - 3*z2*cz1*y[2] + 3*z1*cz2*y[1] - cz3*y[0]
+  ];
 
-  var right = [
-    z3*x[3] - 3*z2*cz*x[2] + 3*z*cz2*x[1] - cz3*x[0],
-    z3*y[3] - 3*z2*cz*y[2] + 3*z*cz2*y[1] - cz3*y[0],
-                    z2*x[3] - 2*z*cz*x[2] + cz2*x[1],
-                    z2*y[3] - 2*z*cz*y[2] + cz2*y[1],
-                                    z*x[3] - cz*x[2],
-                                    z*y[3] - cz*y[2],
-                                                x[3],
-                                                y[3]];
+  const right: bezierUnitedCoordT = [
+    z3*x[3] - 3*z2*cz1*x[2] + 3*z1*cz2*x[1] - cz3*x[0],
+    z3*y[3] - 3*z2*cz1*y[2] + 3*z1*cz2*y[1] - cz3*y[0],
+                    z2*x[3] - 2*z1*cz1*x[2] + cz2*x[1],
+                    z2*y[3] - 2*z1*cz1*y[2] + cz2*y[1],
+                                    z1*x[3] - cz1*x[2],
+                                    z1*y[3] - cz1*y[2],
+                                                  x[3],
+                                                  y[3]
+  ];
 
+  const result = { left, right };
   if (options.fitUnitSquare) {
-    return {
-      left: left.map(function(el, i) {
-        if (i % 2 == 0) {
-          //return el * (1 / left[6])
-          var Xmin = left[0];
-          var Xmax = left[6]; //should be 1
-          var Sx = 1 / (Xmax - Xmin);
-          return (el - Xmin) * Sx;
-        } else {
-          //return el * (1 / left[7])
-          var Ymin = left[1];
-          var Ymax = left[7]; //should be 1
-          var Sy = 1 / (Ymax - Ymin);
-          return (el - Ymin) * Sy;
-        }
-      }),
-      right: right.map(function(el, i) {
-        if (i % 2 == 0) {
-          //xval
-          var Xmin = right[0]; //should be 0
-          var Xmax = right[6];
-          var Sx = 1 / (Xmax - Xmin);
-          return (el - Xmin) * Sx;
-        } else {
-          //yval
-          var Ymin = right[1]; //should be 0
-          var Ymax = right[7];
-          var Sy = 1 / (Ymax - Ymin);
-          return (el - Ymin) * Sy;
-        }
-      })
-    }
+    return fitUnitSquare(result);
   } else {
-   return { left: left, right: right};
+   return result;
   }
 }
