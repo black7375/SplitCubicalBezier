15e320bb53375813785c451f62d3f2b5bccbaeaf
alstjr7375 <alstjr7375@daum.net>
2022-03-12 02:54:50 +0900

    Fix: semicolon to coma

--- src/index.ts
+++ src/index.ts
@@ -30,45 +30,45 @@ interface splicCubicBezierResultI {
 // == Presets ==================================================================
 export const presets = {
 // Name           Func               x1     y1     x2     y2
-  linear:         createCubicBezier( 0   ,  0   ,  1   ,  1   );
-  cubicBezier:    createCubicBezier( 0   ,  1   ,  1   ,  0   );
+  linear:         createCubicBezier( 0   ,  0   ,  1   ,  1   ),
+  cubicBezier:    createCubicBezier( 0   ,  1   ,  1   ,  0   ),
 
-  ease:           createCubicBezier( 0.25,  0.1 ,  0.25,  1   );
-  easeIn:         createCubicBezier( 0.42,  0   ,  1   ,  1   );
-  easeOut:        createCubicBezier( 0   ,  0   ,  0.58,  1   );
-  easeInOut:      createCubicBezier( 0.42,  0   ,  0.58,  1   );
+  ease:           createCubicBezier( 0.25,  0.1 ,  0.25,  1   ),
+  easeIn:         createCubicBezier( 0.42,  0   ,  1   ,  1   ),
+  easeOut:        createCubicBezier( 0   ,  0   ,  0.58,  1   ),
+  easeInOut:      createCubicBezier( 0.42,  0   ,  0.58,  1   ),
 
-  easeInSine:     createCubicBezier( 0.12,  0   ,  0.39,  0   );
-  easeOutSine:    createCubicBezier( 0.61,  1   ,  0.88,  1   );
-  easeInOutSine:  createCubicBezier( 0.37,  0   ,  0.63,  1   );
+  easeInSine:     createCubicBezier( 0.12,  0   ,  0.39,  0   ),
+  easeOutSine:    createCubicBezier( 0.61,  1   ,  0.88,  1   ),
+  easeInOutSine:  createCubicBezier( 0.37,  0   ,  0.63,  1   ),
 
-  easeInQuad:     createCubicBezier( 0.11,  0   ,  0.5 ,  0   );
-  easeOutQuad:    createCubicBezier( 0.5 ,  1   ,  0.89,  1   );
-  easeInOutQuad:  createCubicBezier( 0.45,  0   ,  0.55,  1   );
+  easeInQuad:     createCubicBezier( 0.11,  0   ,  0.5 ,  0   ),
+  easeOutQuad:    createCubicBezier( 0.5 ,  1   ,  0.89,  1   ),
+  easeInOutQuad:  createCubicBezier( 0.45,  0   ,  0.55,  1   ),
 
-  easeInCubic:    createCubicBezier( 0.32,  0   ,  0.67,  0   );
-  easeOutCubic:   createCubicBezier( 0.33,  1   ,  0.68,  1   );
-  easeInOutCubic: createCubicBezier( 0.65,  0   ,  0.35,  1   );
+  easeInCubic:    createCubicBezier( 0.32,  0   ,  0.67,  0   ),
+  easeOutCubic:   createCubicBezier( 0.33,  1   ,  0.68,  1   ),
+  easeInOutCubic: createCubicBezier( 0.65,  0   ,  0.35,  1   ),
 
-  easeInQuart:    createCubicBezier( 0.5 ,  0   ,  0.75,  0   );
-  easeOutQuart:   createCubicBezier( 0.25,  1   ,  0.5 ,  1   );
-  easeInOutQuart: createCubicBezier( 0.76,  0   ,  0.24,  1   );
+  easeInQuart:    createCubicBezier( 0.5 ,  0   ,  0.75,  0   ),
+  easeOutQuart:   createCubicBezier( 0.25,  1   ,  0.5 ,  1   ),
+  easeInOutQuart: createCubicBezier( 0.76,  0   ,  0.24,  1   ),
 
-  easeInQuint:    createCubicBezier( 0.64,  0   ,  0.78,  0   );
-  easeOutQuint:   createCubicBezier( 0.22,  1   ,  0.36,  1   );
-  easeInOutQuint: createCubicBezier( 0.83,  0   ,  0.17,  1   );
+  easeInQuint:    createCubicBezier( 0.64,  0   ,  0.78,  0   ),
+  easeOutQuint:   createCubicBezier( 0.22,  1   ,  0.36,  1   ),
+  easeInOutQuint: createCubicBezier( 0.83,  0   ,  0.17,  1   ),
 
-  easeInExpo:     createCubicBezier( 0.7 ,  0   ,  0.84,  0   );
-  easeOutExpo:    createCubicBezier( 0.16,  1   ,  0.3 ,  1   );
-  easeInOutExpo:  createCubicBezier( 0.87,  0   ,  0.13,  1   );
+  easeInExpo:     createCubicBezier( 0.7 ,  0   ,  0.84,  0   ),
+  easeOutExpo:    createCubicBezier( 0.16,  1   ,  0.3 ,  1   ),
+  easeInOutExpo:  createCubicBezier( 0.87,  0   ,  0.13,  1   ),
 
-  easeInCirc:     createCubicBezier( 0.55,  0   ,  1   ,  0.45);
-  easeOutCirc:    createCubicBezier( 0   ,  0.55,  0.45,  1   );
-  easeInOutCirc:  createCubicBezier( 0.85,  0   ,  0.15,  1   );
+  easeInCirc:     createCubicBezier( 0.55,  0   ,  1   ,  0.45),
+  easeOutCirc:    createCubicBezier( 0   ,  0.55,  0.45,  1   ),
+  easeInOutCirc:  createCubicBezier( 0.85,  0   ,  0.15,  1   ),
 
-  easeInBack:     createCubicBezier( 0.36,  0   ,  0.66, -0.56);
-  easeOutBack:    createCubicBezier( 0.34,  1.56,  0.64,  1   );
-  easeInOutBack:  createCubicBezier( 0.68, -0.6 ,  0.32,  1.6 );
+  easeInBack:     createCubicBezier( 0.36,  0   ,  0.66, -0.56),
+  easeOutBack:    createCubicBezier( 0.34,  1.56,  0.64,  1   ),
+  easeInOutBack:  createCubicBezier( 0.68, -0.6 ,  0.32,  1.6 )
 };
 
 // == Basics ===================================================================
